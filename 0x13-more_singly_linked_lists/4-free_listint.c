28fd1de10e837de8aaa56093a0b205c4a49c76d7
kingsdomain <kingsdomain50@gmail.com>
2023-09-26 19:20:06 -0700

    commit it

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
- * free_listint - Releases memory allocated for a linked list
- * @head: Pointer to the beginning of the list to be freed
- */
+* free_listint - Releases memory allocated for a linked list
+* @head: Pointer to the beginning of the list to be freed
+*/
 void free_listint(listint_t *head)
 {
-        listint_t *temp;
+listint_t *temp;
 
-        for (; head != NULL; head = temp)
-        {
-                temp = head->next;
-                free(head);
-        }
+for (; head != NULL; head = temp)
+{
+temp = head->next;
+free(head);
+}
 }
