d720ac98bf5bfb7428e23f8e8da69027d31aed9e
kingsdomain <kingsdomain50@gmail.com>
2023-09-29 19:22:25 -0700

    Hot fix

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_endianness - Determines whether a machine is big endian or little endian.
+ * get_endianness - Determines if a machine is big or little endian.
  *
  * Return: 0 for big endian, 1 for little endian.
  */
