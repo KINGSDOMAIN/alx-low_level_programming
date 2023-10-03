2bed4d6759b84bb05c1fe4d41197b67aff562fcb
kingsdomain <kingsdomain50@gmail.com>
2023-10-02 18:32:37 -0700

    Commit all

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,33 +1,33 @@
 #include "main.h"
 
 /**
- * create_file - Creates a new file.
- * @filename: A pointer to the name of the file to be created.
- * @text_content: A pointer to a string that will be written to the file.
- *
- * Return: If the function fails, it returns -1.
- *         Otherwise, it returns 1.
- */
+* create_file - Creates a new file.
+* @filename: A pointer to the name of the file to be created.
+* @text_content: A pointer to a string that will be written to the file.
+*
+* Return: If the function fails, it returns -1.
+*         Otherwise, it returns 1.
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int p, x, len = 0;
+int p, x, len = 0;
 
-	if (filename == NULL)
-		return (-1);
+if (filename == NULL)
+return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+if (text_content != NULL)
+{
+for (len = 0; text_content[len];)
+len++;
+}
 
-	p = open(filename, O_WRONLY | O_APPEND);
-	x = write(p, text_content, len);
+p = open(filename, O_WRONLY | O_APPEND);
+x = write(p, text_content, len);
 
-	if (p == -1 || x == -1)
-		return (-1);
+if (p == -1 || x == -1)
+return (-1);
 
-	close(p);
+close(p);
 
-	return (1);
+return (1);
 }
