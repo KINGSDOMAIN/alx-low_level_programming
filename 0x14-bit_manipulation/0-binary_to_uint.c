d73587b5ef3e8ae136ecea3a393a30b86fa20c56
kingsley <kingsdomain50@gmail.com>
2023-11-24 09:10:36 -0800

    Hot FIx

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -23,17 +23,16 @@ unsigned int binary_to_uint(const char *b)
 	for (len = 0; b[len] != '\0'; len++)
 		if (len == 1 && (b[0] == '0' || b[0] == '1'))
 			return (b[0] - 48);
-	for (i = 0; b[i] != '0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
-			return (0);
+		return (0);
 		for (j = len - 1; j > 0; j--)
-		{
-			pow = pow * base;
-			sum = sum + (pow * (b[i] - 48));
-			len--;
-			pow = 1;
-		}
+		pow = pow * base;
+		sum = sum + (pow * (b[i] - 48));
+		len--;
+		pow = 1;
+	
 	}
 	return (sum);
 }
